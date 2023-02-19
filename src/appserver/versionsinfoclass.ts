86b9a734194b970b13eba48e7833d8221620714b
Peter Hornburger <p.hornburger@gmail.com>
2023-02-19 10:55:32 +0100

    Sonntag 10:55

--- src/appserver/versionsinfoclass.ts
+++ src/appserver/versionsinfoclass.ts
@@ -2,7 +2,7 @@
 class VersionsinfoClass {
 
   public Verion: number = 0.04;
-  public Versionsdatum: string = '19.02.2023 10:37';
+  public Versionsdatum: string = '19.02.2023 10:55';
 }
 
 export {VersionsinfoClass}
