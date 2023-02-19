d1113db8524dfecbcd406ea0b5bc6b4d2e20f246
Peter Hornburger <p.hornburger@gmail.com>
2023-02-19 18:56:17 +0100

    Sontag 19:00

--- src/routes/homeroutes.ts
+++ src/routes/homeroutes.ts
@@ -2,13 +2,12 @@ import express, {Application, Request, Response, Router} from 'express';
 import {VersionsinfoClass} from '../versionsinfoclass';
 import {DebugClass} from "../debug";
 import {ConnectionClass} from "../connectionclass";
-import {Configclass} from "../configclass";
+import config from "config";
 
 const Info             = new VersionsinfoClass();
 const Connection       = new ConnectionClass();
 const app: Application = express();
 const Environment      = app.get('env');
-const Config           = new Configclass();
 
 class HomerouterClass {
 
@@ -40,7 +39,10 @@ class HomerouterClass {
           <td>Environment from Express (env):</td><td>${Environment}</td>
       </tr>
       <tr>
-          <td>Environment from Environment (NODE_ENV):</td><td>${process.env.NODE_ENV}</td>
+          <td>Environment from  (env):</td><td>${process.env.NODE_ENV}</td>
+      </tr>
+      <tr>
+          <td>Environment from Environment (NODE_ENV):</td><td>${config.util.getEnv('NODE_ENV')}</td>
       </tr>
       <tr>
           <td>DB Unsername:</td><td>${Connection.COSMOSDB_USER}</td>
@@ -49,10 +51,10 @@ class HomerouterClass {
           <td>DB Passwort:</td><td>${Connection.COSMOSDB_PASSWORD}</td>
       </tr>
       <tr>
-          <td>DB Passwort from config Environment (gespiegelt):</td><td>${Config.get('passwort')}</td>
+          <td>DB Passwort from config Environment (gespiegelt):</td><td>${config.get('passwort')}</td>
       </tr>
       <tr>
-          <td>DB Passwort from config Environment (env abfrage intern config):</td><td>${Config.getEnv('ALLOW_CONFIG_MUTATIONS')}</td>
+          <td>DB Passwort from config Environment (env abfrage intern config):</td><td>${config.util.getEnv('ALLOW_CONFIG_MUTATIONS')}</td>
       </tr>
       </table>
       </body>
