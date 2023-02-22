128dfa541063123b32c34c397fa8cc421989eea5
Peter Hornburger <p.hornburger@gmail.com>
2023-02-22 16:57:54 +0100

    Mittwoch 17:00

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ if(typeof process.env.NODE_ENV === 'undefined') {
 }
 
 import express, {NextFunction, Request, Response} from "express";
-import helmet from "helmet";
-import morgan from "morgan";
+// import helmet from "helmet";
+// import morgan from "morgan";
 import { HomerouterClass } from './routes/homeroutes';
 import { StandorterouterClass } from './routes/standorterouts';
 import { Application } from "express";
@@ -37,10 +37,31 @@ const Projektpunkterouter: ProjektpunkteroutsClass = new ProjektpunkteroutsClass
 const Protokollrouter: ProtokolleroutsClass = new ProtokolleroutsClass();
 const Config: ConfigClass = new ConfigClass();
 
+let NODE_ENV: string       = config.has('node_env')        ? config.get('node_env')        : 'none';
+let Statausmessage: string = config.has('Statusmessage')   ? config.get('Statusmessage')   : 'none';
+let User : string          = config.has('db_user')         ? config.get('db_user')         : 'none';
+let Passwort: string       = config.has('db_password')     ? config.get('db_password')     : 'none';
+let DBName: string         = config.has('COSMOSDB_DBNAME') ? config.get('COSMOSDB_DBNAME') : 'none';
+let DBHost: string         = config.has('COSMOSDB_HOST')   ? config.get('COSMOSDB_HOST')   : 'none';
+let DBPort: string         = config.has('COSMOSDB_PORT')   ? config.get('COSMOSDB_PORT')   : 'none';
+let SecretKey: string      = config.has('secretkey')       ? config.get('secretkey')       : 'none';
+
+Config.Init(
+  NODE_ENV,
+  Statausmessage,
+  User,
+  Passwort,
+  DBName,
+  DBHost,
+  DBPort,
+  SecretKey
+);
+
+Connection.Init(Config);
+Homerouter.Init(Config);
 
 
-
-app.use(morgan('dev')); // http request Debug messages
+// app.use(morgan('dev')); // http request Debug messages
 app.use((req: Request, res: Response, next: NextFunction) => {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -61,7 +82,7 @@ Protokollrouter.SetRoutes();
 
 app.use(express.json()); // setze request.body JSON
 app.use(express.urlencoded({extended: true}));
-app.use(helmet());
+// app.use(helmet());
 
 app.use('/',              Homerouter.homerouter);
 app.use('/standorte',     Standorterouter.standorterouter);
@@ -76,29 +97,6 @@ app.listen(port, () => {
 
   Debug.ShowInfoMessage(`Cockpit Server is listening on port ${port}.....`, 'index.ts', 'Server');
   Debug.ShowInfoMessage(`Startup time ${moment().format('HH:mm:ss')}`, 'index.ts', 'Server');
-
-  let NODE_ENV: string       = config.has('node_env')        ? config.get('node_env')        : 'none';
-  let Statausmessage: string = config.has('Statusmessage')   ? config.get('Statusmessage')   : 'none';
-  let User : string          = config.has('db_user')         ? config.get('db_user')         : 'none';
-  let Passwort: string       = config.has('db_password')     ? config.get('db_password')     : 'none';
-  let DBName: string         = config.has('COSMOSDB_DBNAME') ? config.get('COSMOSDB_DBNAME') : 'none';
-  let DBHost: string         = config.has('COSMOSDB_HOST')   ? config.get('COSMOSDB_HOST')   : 'none';
-  let DBPort: string         = config.has('COSMOSDB_PORT')   ? config.get('COSMOSDB_PORT')   : 'none';
-  let SecretKey: string      = config.has('secretkey')       ? config.get('secretkey')       : 'none';
-
-  Config.Init(
-    NODE_ENV,
-    Statausmessage,
-    User,
-    Passwort,
-    DBName,
-    DBHost,
-    DBPort,
-    SecretKey
-  );
-
-  Connection.Init(Config);
-  Homerouter.Init(Config);
 
   /*
 
