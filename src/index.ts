8c881e59e91b26c9b1f1062a098372fe49fb273b
Peter Hornburger <p.hornburger@gmail.com>
2023-03-05 08:19:40 +0100

    Sonntag 08:20

--- src/index.ts
+++ src/index.ts
@@ -74,11 +74,11 @@ Connection.Init(Config);
 Homerouter.Init(Config);
 
 // No 'Access-Control-Allow-Origin' header is present on the requested resource
-//   res.setHeader('Access-Control-Allow-Origin',  '*');
 
 
 app.use((req: Request, res: Response, next: NextFunction) => {
 
+  res.setHeader('Access-Control-Allow-Origin',  '*');
   res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
 
