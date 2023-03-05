f8728cdcdd906dc36c444881240d803064bc860b
Peter Hornburger <p.hornburger@gmail.com>
2023-03-05 15:03:03 +0100

    Sonntag 15:00

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Allow-Origin',  '*');
   res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-
   next();
 });
 
