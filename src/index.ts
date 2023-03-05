c6bf3cc51b446c3884f5a5f5ae8d1d1b22d0e078
Peter Hornburger <p.hornburger@gmail.com>
2023-03-05 14:25:52 +0100

    Sonntag 14:30

--- src/index.ts
+++ src/index.ts
@@ -74,12 +74,15 @@ Homerouter.Init(Config);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
 
+
   res.setHeader('Access-Control-Allow-Origin',  '*');
   res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
 
   next();
 });
+
+console.log('Hello World');
 
 Homerouter.SetRoutes();
 Standorterouter.SetRoutes();
